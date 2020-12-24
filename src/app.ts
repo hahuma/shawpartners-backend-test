18d7b6dd8a14a1e59d9bf28510b65c2bb4cd0b6e
hahuma <firminopetterson@gmail.com>
2020-12-24 14:26:58 -0300

    added eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,28 @@
-import express, { Express } from 'express'
-import cors from 'cors'
+import express, { Express } from "express"
+import cors from "cors"
 
-import routes from './routes'
-
+import routes from "./routes"
 
 const corsOptions = {
-    origin: process.env.APP_URL|| undefined
+  origin: process.env.APP_URL || undefined,
 }
-
 class App {
-    public express: Express
+  public express: Express
 
-    constructor() {
-        this.express = express()
-        this._middlewares()
-        this._routes()
-    }
+  constructor() {
+    this.express = express()
+    this._middlewares()
+    this._routes()
+  }
 
-    private _middlewares() {
-        this.express.use(express.json())   
-        this.express.use(cors(corsOptions))
-    }
+  private _middlewares() {
+    this.express.use(express.json())
+    this.express.use(cors(corsOptions))
+  }
 
-    private _routes() {
-        this.express.use(routes)
-    }
+  private _routes() {
+    this.express.use(routes)
+  }
 }
 
 export default new App().express
